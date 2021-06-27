417c08bc171d20e4b1ad885d6c72515782b0b4cf
Skwizz <72879590+Skwizzz@users.noreply.github.com>
2021-06-27 20:46:35 +0200

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,9 @@
-print("hello world!")
+import random
+n = random.randint(0,10)
+if random < 10 and > 0:
+  printer = True
+if printer = True:
+  while n < 10:
+    n += 1
+if n>10:
+  print("Hello World !")
